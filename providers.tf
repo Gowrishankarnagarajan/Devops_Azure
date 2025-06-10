6370e759a3dacf421910c744050af3085edfeaba
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-06-10 17:27:14 +0100

    Test1

--- providers.tf
+++ providers.tf
@@ -1,30 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.53.0"
-    }
 
-    github = {
-      source  = "integrations/github"
-      version = "5.25.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-
-  cloud {
-    organization = "TERRAFORM-ORG-NAME"
-
-    workspaces {
-      name = "TERRAFORM-WORKSPACE-NAME"
-    }
-  }
-
-}
 
 provider "azurerm" {
   subscription_id = var.subscription_id
