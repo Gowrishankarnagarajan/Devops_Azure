89849956f0700d7ed8b3ab1b712fc1fb0367692c
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-06-11 08:23:53 +0100

    Output File added

--- main.tf
+++ main.tf
@@ -57,3 +57,22 @@ resource "random_id" "frontend" {
 resource "random_id" "backend" {
   byte_length = 4
 } 
+# Output the web app URLs
+output "frontend_web_app_url" {
+  value = azurerm_linux_web_app.frontend.default_site_hostname
+}   
+output "backend_web_app_url" {
+  value = azurerm_linux_web_app.backend.default_site_hostname
+}
+# Output the resource group name
+output "resource_group_name" {
+  value = azurerm_resource_group.gs.name
+} 
+# Output the service plan ID
+output "service_plan_id" {
+  value = azurerm_service_plan.gs.id
+}
+# Output the location of the resource group
+output "resource_group_location" {
+  value = azurerm_resource_group.gs.location
+}   
