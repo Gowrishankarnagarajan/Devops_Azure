c2ec9a98a892427c8828730531ef6fe0a2544d5f
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-06-12 16:06:09 +0100

    App1 applicaction Fic the erroe to set the Depends on forf SCM

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "azurerm_app_service_source_control" "scm" {
   app_id            = azurerm_linux_web_app.as1.id
   branch            = "main"
   repo_url          = "https://github.com/Gowrishankarnagarajan/Invitation"
-  
+  depends_on = [ azurerm_linux_web_app.as1 ]
 }
 resource "azurerm_linux_web_app" "as2" {
   name                = "${var.prefix}-webapp2"
