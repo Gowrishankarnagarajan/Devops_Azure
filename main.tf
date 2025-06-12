9e00d47367c77ebe5266b15a40688565103eb741
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-06-12 14:29:58 +0100

    Adding the source for the applicaction

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-variable "prefix" {
-  description = "Prefix for resource names"
-  default     = "gs"
-  type =  string
-  
-}
-
-
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-rg"
   location = "West Europe"
@@ -36,14 +28,14 @@ resource "azurerm_app_service_source_control" "scm" {
   repo_url          = "https://github.com/Gowrishankarnagarajan/Invitation"
   
 }
-resource "azurerm_linux_web_app" "backend" {
-  name                = "backend-webapp-${random_id.backend.hex}"
-  location            = azurerm_resource_group.gs.location
-  resource_group_name = azurerm_resource_group.gs.name
-  service_plan_id     = azurerm_service_plan.gs.id
+resource "azurerm_linux_web_app" "aps2" {
+  name                = "${var.prefix}-webapp2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
-    always_on = false
+ 
   }
 }
 
