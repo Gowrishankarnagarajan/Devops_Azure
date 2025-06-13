82c347dd88014ce6e549494abb524fb7da3c60f4
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-06-13 09:40:20 +0100

    Change the Locaction

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Use the new resource type
 resource "azurerm_service_plan" "asp" {
-  name                = "azurerm_service_plan"
+  name                = "${var.prefix}-asp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
