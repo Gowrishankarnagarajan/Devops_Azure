ae51d1e7620cc55b3934880b132c5858e75dcee1
Gowrishankarnagarajan <gowrishankarn.2096@gmail.com>
2025-06-13 11:36:16 +0100

    Removing SCM

--- main.tf
+++ main.tf
@@ -22,16 +22,17 @@ resource "azurerm_linux_web_app" "as1" {
   site_config {
 
     always_on = false
-
+    
      }
 } 
 
-resource "azurerm_app_service_source_control" "scm" {
-  app_id            = azurerm_linux_web_app.as1.id
-  branch            = "main"
-  repo_url          = "https://gowrishankarnagarajan.github.io/Invitation/"
-  depends_on = [ azurerm_linux_web_app.as1 ]
-}
+#resource "azurerm_app_service_source_control" "scm" {
+ # app_id            = azurerm_linux_web_app.as1.id
+  #branch            = "master"
+  #repo_url          = "https://github.com/Gowrishankarnagarajan/python-docs-hello-world"
+  #depends_on = [ azurerm_linux_web_app.as1 ]
+#}
+
 resource "azurerm_linux_web_app" "as2" {
   name                = "${var.prefix}-webapp2"
   location            = azurerm_resource_group.rg.location
