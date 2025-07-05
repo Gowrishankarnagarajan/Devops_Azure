ef622e505bc08e5a1db16e394836b79c3e5e8fd8
Gowrishankarnagarajan <93309610+Gowrishankarnagarajan@users.noreply.github.com>
2025-07-05 21:04:35 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,20 +1,20 @@
+# 
 
-
-provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  features {}
-}
-# This Terraform configuration sets up an Azure App Service with a resource group, app service plan, and two web apps (frontend and backend).
-terraform { 
-  cloud { 
+# provider "azurerm" {
+#   subscription_id = var.subscription_id
+#   client_id       = var.client_id
+#   client_secret   = var.client_secret
+#   tenant_id       = var.tenant_id
+#   features {}
+# }
+# # This Terraform configuration sets up an Azure App Service with a resource group, app service plan, and two web apps (frontend and backend).
+# terraform { 
+#   cloud { 
     
-    organization = "gs-devops" 
+#     organization = "gs-devops" 
 
-    workspaces { 
-      name = "Devops" 
-    } 
-  } 
-}
+#     workspaces { 
+#       name = "Devops" 
+#     } 
+#   } 
+# # 
